220a155106372216bfa0e04d1d08b35e72afbf81
jyoti sangwan <jyotisangwan@jyotis-Air.lan>
2025-11-17 18:56:35 +0530

    password hashing

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,11 +7,18 @@ const router = express.Router()
 
 router.post('/register', (req, res) => {
   const {username, password} = req.body
-  console.log(username, password)
+  const hashedpass = bcrypt.hashSync(password, 8)
+  console.log(hashedpass)
   res.sendStatus(201)
 })
 
 router.post('/login', (req, res) => {
+  //password stored in db by brcrypt is always in encrypted form 
+  //so we cant directly match the pass enetered by th euser 
+  //we will encrypt the password enetered by user and then match it
+  
+  const {username, password} = req.body
+  console.log(username, password)
   res.send("Login route working")
 })
 
